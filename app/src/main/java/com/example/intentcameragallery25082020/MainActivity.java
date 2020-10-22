fa2ede37e44cec64d3ed242e8137055e65aaa297
phatdroid94 <phatdroid94@gmail.com>
2020-10-22 20:10:00 +0700

    checkselfpermission

--- app/src/main/java/com/example/intentcameragallery25082020/MainActivity.java
+++ app/src/main/java/com/example/intentcameragallery25082020/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.intentcameragallery25082020;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.intentcameragallery25082020.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding mBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        mBinding.buttonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+
+                }
+            }
+        });
     }
 }
